eb4fa40c80cfe51884cfb80805ef1d07c9b9a03e
Leonardo (NoteMauricy) <leorick97@gmail.com>
2019-06-20 17:06:18 -0300

    Create the project. Inclusion of Employee class

---
+++ Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeRegister.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() { }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
